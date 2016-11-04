4420bb26f3c650fc813e08ffd165a9e4ed740713
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-04 13:08:29 -0500

    1:08

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,6 +1,7 @@
 describe Viking do 
 
   let(:viking){ Viking.new }
+  let(:bow){ double("Bow", is_a?: true ) }
 
   it "has a name when you pass it a name" do 
     expect(Viking.new("Bob").name).to eq("Bob")
@@ -20,11 +21,52 @@ describe Viking do
 
   describe "#pick_up_weapon" do 
     
-    let(:weapon){ double("Weapon", is_a?: true ) }
+    let(:axe){ double("Axe", is_a?: true, klass: Axe )}
+    let(:non_weapon){ double( is_a?: false )}
 
     it "can pick up a weapon" do 
-      viking.pick_up_weapon(weapon)
-      expect(viking.weapon).to eq(weapon)
+      viking.pick_up_weapon(bow)
+      expect(viking.weapon).to eq(bow)
     end
+
+    it 'can\'t pick up a non-weapon' do
+      expect{viking.pick_up_weapon(non_weapon)}.to raise_error("Can't pick up that thing")
+    end
+
+    it 'replaces weapon when it picks a new weapon up' do
+      viking = Viking.new("Randy", 80, 5, bow)
+      viking.pick_up_weapon(axe)
+      expect(viking.weapon.klass).to eq(Axe)
+    end
+
   end
+
+  describe '#drop_weapon' do
+
+    it 'drops the weapon' do
+      viking.pick_up_weapon(bow)
+      viking.drop_weapon
+      expect(viking.weapon).to be_nil
+    end
+
+  end
+
+  describe '#receive_attack' do
+
+    it 'reduces the viking\'s health by the damage dealt' do
+      allow(viking).to receive(:puts)
+      expect{viking.receive_attack(10)}.to change{viking.health}.by(-10)
+    end
+
+    it 'should call the take_damage method' do
+      expect(viking).to receive(:take_damage)
+      viking.receive_attack(20)
+    end
+
+  end
+
+
+
+
+
 end
